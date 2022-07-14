2986c03784e40828f7e949db02c6a371ff13e19d
Gauri2230 <108929783+Gauri2230@users.noreply.github.com>
2022-07-14 17:04:48 +0530

    Add files via upload

---
+++ Widening.java
@@ -0,0 +1,11 @@
+class Widening{
+	public static void main(String args[])
+	{
+		int num1=10;
+		int num2=78;
+		double no1=(double) num1;
+		double no2=(double) num2;
+		System.out.println("Double value of num1="+no1);
+		System.out.println("Double value of num2="+no2);
+	}
+ }
