a5777cddf586e7838b50236fa83cfb742f4e6eac
Gauri2230 <108929783+Gauri2230@users.noreply.github.com>
2022-07-14 16:29:09 +0530

    Add files via upload

---
+++ Narrowing.java
@@ -0,0 +1,12 @@
+class Narrowing
+{
+ 	public static void main(String args[])
+	{
+		double a=10.46;
+		double b=7.89;
+		int c=(int) a;
+		int d=(int) b;
+		System.out.println("Integer value of a="+c);
+		System.out.println("Integer value of b="+d);
+	}
+}
